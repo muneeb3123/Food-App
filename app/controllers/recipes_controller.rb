b8da2bb7ef0fce48d804e0d7868607350c3277a9
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-23 04:28:54 +0500

    create controller for recipe

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,36 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @user = current_user
+    @recipe = @user.recipes.new(recipe_params)
+    if @recipe.save
+      flash[:notice] = 'Recipe created successfully'
+      redirect_to recipes_path
+    else
+      flash[:notice] = 'Recipe not created'
+      render :new
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
+  end
+end
