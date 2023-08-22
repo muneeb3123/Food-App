fffaec8d35fd0df679e8bee3d4d9abc3e6e9fa0d
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-08-22 17:12:05 -0400

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
 
-    resources :foods  do
-      resources :recipe_foods
-    end
-    resources :recipes do
-      resources :recipe_foods
-    end
+  resources :foods do
+    resources :recipe_foods
+  end
+  resources :recipes do
+    resources :recipe_foods
+  end
 end
