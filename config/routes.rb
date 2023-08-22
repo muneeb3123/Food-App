e29fd21c9fcb72ce9bf810fb3aaa7cd53b5e4b4a
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-22 06:44:36 +0500

    add routes for all models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  root 'users#index'
   devise_for :users
+
+  resources :users do
+    resources :foods do
+      resources :recipe_foods
+    end
+    resources :recipes do
+      resources :recipe_foods
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
