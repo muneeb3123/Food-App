8dbf8e23d11462dff850c69f90b2899a21514d8a
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-25 05:13:40 +0500

    add edit and delete

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -4,6 +4,30 @@ class RecipeFoodsController < ApplicationController
     @recipe_food = RecipeFood.new
   end
 
+  def destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.find(params[:id])
+    @recipe_food.destroy
+    redirect_to recipe_path(@recipe), notice: 'successfully remove'
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.find(params[:id])
+  end
+
+  def update
+    @recipe_food = RecipeFood.find(params[:id])
+
+    if @recipe_food.update(recipe_food_params)
+      flash[:notice] = 'Updated successfully'
+      redirect_to recipe_path(@recipe_food.recipe)
+    else
+      flash[:notice] = 'Not Updated'
+      render :edit
+    end
+  end
+
   def create
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
@@ -20,6 +44,10 @@ class RecipeFoodsController < ApplicationController
   private
 
   def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, :food_id, :recipe_id)
+  end
+
+  def update_params
     params.require(:recipe_food).permit(:quantity, :food_id, :recipe_id)
   end
 end
