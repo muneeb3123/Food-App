310eced867ba14957dea2453e43fa6f0ece18afc
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-08-24 23:07:59 -0400

    Add user model test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # create a subjet user
+  subject { User.new(name: 'Test User', email: 'test@mail.com', role: 'admin') }
+
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'email should be present' do
+    subject.email = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'role should be present' do
+    subject.role = nil
+    expect(subject).not_to be_valid
+  end
+end
