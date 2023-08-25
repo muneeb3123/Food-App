91351cfe0d5ce67fd2713a998d99d9b1bb89841d
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-08-25 14:04:02 -0400

    Fix linter errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,7 +36,7 @@ RSpec.configure do |config|
   # config.include Devise::Test::ControllerHelpers, type: :controller
   # config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Devise::Test::IntegrationHelpers, type: :request  
+  config.include Devise::Test::IntegrationHelpers, type: :request
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{Rails.root}/spec/fixtures"
